1f2cc238f5d6158d054adedd41c69ffba77ace16
snownisha <snownisha123@gmail.com>
2022-08-22 09:51:53 +0200

    Lists (Append, Insert, remove, Pop etc..)

---
+++ Lists.py
@@ -0,0 +1,12 @@
+name = ['Snow', 'Nisha', 'Doss']
+print(name)
+name.append('Nirmal')
+name.insert(2, 'Steffi')
+name.remove('Snow')
+print(name)
+
+values = [9.5, 'Nisha' , "Hello World"]
+print(values)
+
+combine = [name, values]
+print(combine)
