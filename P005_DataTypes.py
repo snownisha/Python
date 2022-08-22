c9515f02ce697684fd230cb51d8c003ad181031c
snownisha <snownisha123@gmail.com>
2022-08-22 11:51:34 +0200

    Data Types

---
+++ P005_DataTypes.py
@@ -0,0 +1,29 @@
+
+#Numeric
+num= 2.5
+print(type(num))
+
+num= 2
+print(type(num))
+
+num= 2+5j
+print(type(num))
+
+a = 5.5
+b = int(a)
+print(b)
+print(float(b))
+print(complex(a))
+
+a = 16
+b=7
+print(bool(b>a))
+
+#range
+range(10)
+print(list(range(10)))
+print(list(range(2,10,2)))
+
+#Dictionary
+dict= {'Snow' :24, 'Nirmal': 32}
+print(dict)
