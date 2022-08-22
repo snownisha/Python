fda2ce80a92f7cf9309fc7d002ca6f634b8fa731
snownisha <snownisha123@gmail.com>
2022-08-22 11:54:52 +0200

    Arithmetic, Assignment, Logical, Relational & Unary Operators

---
+++ P006_Operators.py
@@ -0,0 +1,25 @@
+#Arithmetic
+x=2
+y=3
+print(x+y)
+
+#Assignment
+x = 5
+x += 3
+print(x)
+
+#Relational
+x=2
+y=3
+print(y==x)
+print(y!=x)
+
+#Logical
+x=2
+y=3
+print((y==x) or (x<y))
+print((y==x) and (x<y))
+
+#Unary
+n=7
+print(-n)
