bb8721dd73ea91e0c7de9367b87803d046ab3b72
snownisha <snownisha123@gmail.com>
2022-08-23 08:40:36 +0200

    User Input

---
+++ P010_UserInput.py
@@ -0,0 +1,8 @@
+FirstName= input("what is your first name: ")
+SecondName= input("what is your second name: ")
+print("Hi " + FirstName + " " + SecondName)
+
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+multi = int(num1 * num2)
+print(" multiplication of 2 number is :" + str(multi))
