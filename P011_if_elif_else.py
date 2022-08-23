0bab48f398ca5038f1d935102d5edba310cf8fa5
snownisha <snownisha123@gmail.com>
2022-08-23 08:41:12 +0200

    If, Elif & Else

---
+++ P011_if_elif_else.py
@@ -0,0 +1,22 @@
+num1 = input("num1: ")
+num2 = input("num2: ")
+
+if(num2>num1):
+    print("num2 is greater")
+
+else:
+    print("num1 is greater")
+print("Auf Wiedersehen")
+
+#elif
+
+x = 9
+if x == 1:
+    print("Its One")
+elif(x==2) :
+    print("Its two")
+elif (x == 3):
+    print("Its Three")
+else:
+    print("Wrong Input")
+
