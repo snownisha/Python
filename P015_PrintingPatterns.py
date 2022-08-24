60f28818a2197a9a68f994e2835777ab41a92071
snownisha <snownisha123@gmail.com>
2022-08-24 09:28:04 +0200

    Printing Patterns

---
+++ P015_PrintingPatterns.py
@@ -0,0 +1,16 @@
+for i in range (4):
+    for j in range (4):
+        print('# ', end="")
+    print()
+
+for i in range (5):
+    for j in range (i):
+        print('# ', end="")
+    print()
+
+
+print()
+for i in range (4):
+    for j in range(4-i):
+        print('# ', end="")
+    print()
