9100bce43fd34ca433a7a4bce34eae1cfa7e4fb4
snownisha <snownisha123@gmail.com>
2022-08-24 09:32:30 +0200

    Instance and Class Variable

---
+++ P019_InstanceAndClassVariable.py
@@ -0,0 +1,17 @@
+class subjects:
+    TotalMarks = 100
+
+    def __init__(self):
+        self.lang = 'English'
+        self.location = 'Germany'
+
+
+
+adsp = subjects()
+acc = subjects()
+ubiq = subjects()
+
+acc.TotalMarks= 90
+subjects.TotalMarks = 50
+
+print (adsp.lang, acc.TotalMarks, ubiq.TotalMarks)
