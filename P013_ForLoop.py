bb7f59586a4a8cf221785b302a4db19ee3abf08f
snownisha <snownisha123@gmail.com>
2022-08-24 09:26:13 +0200

    For Loop

---
+++ P013_ForLoop.py
@@ -0,0 +1,10 @@
+#For printing the lines separately
+x = ['Nisha', 24, 'Offenburg']
+
+for i in x:
+    print(i)
+
+#Printing the numbers which is not divisible by 5
+for i in range (1,21):
+    if (i%5 != 00):
+        print(i)
