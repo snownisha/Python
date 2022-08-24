808e54cd619519fb8437e1673995d454d22dbf1a
snownisha <snownisha123@gmail.com>
2022-08-24 09:30:39 +0200

    __InitMethod(self)

---
+++ P017_InitMethod.py
@@ -0,0 +1,20 @@
+class city:
+    def __init__(self, city, district):
+        self.city = city
+        self.district = district
+
+
+
+    def speci(self):
+        print(self.city, self.district)
+
+
+city1 = city('Chennai', 'Chennai')
+city2 = city('Palakurichy', 'Trichy')
+
+city1.speci()
+city2.speci()
+
+
+
+
