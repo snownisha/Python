d521baa87c581a777e4bf9fe2b5643ca8822b43f
snownisha <snownisha123@gmail.com>
2022-08-24 09:31:23 +0200

    Comparing Objects

---
+++ P018_ComparingObjects.py
@@ -0,0 +1,29 @@
+class computer:
+    def __init__(self):
+        self.name = "Snow"
+        self.age = 24
+
+
+    def compare(self, other):
+        if self.age == other.age:
+            return True
+        else:
+            return False
+
+
+c1 = computer()
+c1.age= 100
+c2 = computer()
+
+if c1.compare(c2):
+    print("They are same")
+else:
+    print("Not Same")
+
+c1.name = "Nisha"
+
+
+print(c1.name)
+print(c1.age)
+print(c2.age)
+print(c2.name)
