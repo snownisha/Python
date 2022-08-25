317ddd97dba141f74db8e9efc379b1fa135bcaa9
snownisha <snownisha123@gmail.com>
2022-08-25 09:23:44 +0200

    Inheritance (Single, Multiple & Multi level)

---
+++ P022_Inheritance.py
@@ -0,0 +1,31 @@
+class Music:
+    lang = 'English'
+    Color = 'Green'
+
+    def __init__(self):
+        print("Inheritance in Python")
+
+M1 = Music()
+
+print(M1.lang)
+
+class MusicPlayer:
+    Songs = 100;
+    def __init__(self):
+        print("parent class")
+
+MP1 = MusicPlayer()
+print(MP1.Songs)
+
+class Spotify (MusicPlayer,Music):
+
+    def __init__(self):
+        print("Child Class- Single Inheritance")
+
+
+spotify1 = Spotify()
+spotify1. quality = 'Good'
+
+
+print(spotify1.Songs, spotify1.quality)
+print(spotify1.lang)
