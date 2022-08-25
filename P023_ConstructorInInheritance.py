93b7aa4c98e93166b0650cfa125b2682e727e65f
snownisha <snownisha123@gmail.com>
2022-08-25 09:24:48 +0200

    Constructor in Inheritance

---
+++ P023_ConstructorInInheritance.py
@@ -0,0 +1,33 @@
+class A:
+
+    def __init__(self):
+        print ("in A init")
+
+    def feature1(self):
+        print("Feature1 is working")
+
+    def feature2(self):
+        print("Feature2 is working")
+
+
+class B(A):
+    def __init__(self):
+        print ("in B init")
+
+    def feature3(self):
+        print("Feature3 is working")
+
+    def feature4(self):
+        print("Feature4 is working")
+
+class C(B):
+    def __init__(self):
+        print ("in C init")
+
+    def feature5(self):
+        super().feature2()
+        print("Feature5 is working")
+
+a1= A()
+b1= B()
+c1 = C()
