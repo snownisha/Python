9bd45a05338eb9cf8bdf2f85ddee54be4a306e23
snownisha <snownisha123@gmail.com>
2022-08-26 09:55:34 +0200

    Operator Overloading

---
+++ P027_Operator Overloading.py
@@ -0,0 +1,20 @@
+class stud:
+
+    def __init__(self, m1,m2):
+        self.m1 = m1
+        self.m2 = m2
+
+    def add(self, other):
+        m1 = self.m1 + other.m1
+        m2 = self.m2 + other.m2
+        s3 = stud(m1,m2)
+
+        return s3
+
+
+s1 = stud(99,78)
+s2 = stud(100,23)
+
+
+s3 = s1 + s2
+print(s3.m1)
