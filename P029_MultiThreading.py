7c61f16e223dc2957ded42b809fc2ae3810ae0b8
snownisha <snownisha123@gmail.com>
2022-08-26 09:56:52 +0200

    Multi Threading

---
+++ P029_MultiThreading.py
@@ -0,0 +1,24 @@
+from threading import *
+from time import sleep
+
+class Hello(Thread):
+    def run(self):
+        for i in range(5):
+            print("Hello")
+sleep(1)
+
+class Hi(Thread):
+    def run(self):
+        for i in range(5):
+            print("Hi")
+
+p1 = Hello()
+p2 = Hi()
+
+p1.start()
+sleep(0.1)
+p2.start()
+
+p1.join()
+p2.join()
+print("Bye......................................")
