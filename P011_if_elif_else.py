40dbd016ec7dc998cc9a355666d8adcdec957969
snownisha <snownisha123@gmail.com>
2022-08-31 06:24:02 +0200

    If_elif_else

--- P011_if_elif_else.py
+++ P011_if_elif_else.py
@@ -1,22 +1,8 @@
-num1 = input("num1: ")
-num2 = input("num2: ")
+name = "Nisha"
 
-if(num2>num1):
-    print("num2 is greater")
-
+if name == "Snow":
+    print("Wish her All the best")
+elif name== "Nirmal":
+    print("Wish her All the best")
 else:
-    print("num1 is greater")
-print("Auf Wiedersehen")
-
-#elif
-
-x = 9
-if x == 1:
-    print("Its One")
-elif(x==2) :
-    print("Its two")
-elif (x == 3):
-    print("Its Three")
-else:
-    print("Wrong Input")
-
+    print("Bye")
