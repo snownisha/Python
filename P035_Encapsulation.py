ba19aedbd8a065aa3b43680ece21f0c0b8dde857
snownisha <snownisha123@gmail.com>
2022-08-31 08:28:51 +0200

    Encapsulation

---
+++ P035_Encapsulation.py
@@ -0,0 +1,13 @@
+class Account:
+    def __init__(self):
+        self.setBalance = 0
+
+    def setBalance(self, balance):
+        self.__balance = balance
+
+    def getBalance(self):
+        print("No Balance")
+
+
+Account1 = Account()
+Account1.getBalance()
