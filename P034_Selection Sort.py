2bd3adea586fdc4dc68763a41b2e5d98b6935387
snownisha <snownisha123@gmail.com>
2022-08-31 08:19:30 +0200

    Selection Sort

---
+++ P034_Selection Sort.py
@@ -0,0 +1,15 @@
+def sort(nums):
+    for i in range(5):
+        minpos = i
+        for j in range  (i, 6):
+            if nums[j]<nums[minpos]:
+                minpos=j
+
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+
+
+nums = [5, 7, 9, 1, 2, 3]
+sort(nums)
+print(nums)
