33cfb39ac702dd50f4b2bea77c25b805a87867d7
snownisha <snownisha123@gmail.com>
2022-09-02 10:57:21 +0200

    Removing Characters from string

---
+++ P039_Removing Characters from string.py
@@ -0,0 +1,12 @@
+
+def remove_chars4(word,index):
+    x = word[4:]
+    return x
+
+def remove_chars2(word,index):
+    x = word[2:]
+    return x
+
+print(remove_chars4("Python", 4))
+print(remove_chars2("Nisha", 2))
+
