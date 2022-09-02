35efcf8a900346005a173e135b50e50d2e46ac43
snownisha <snownisha123@gmail.com>
2022-09-02 10:55:21 +0200

    Print the sum of the current number and the previous number

---
+++ P037_Sum of Current and Previous Number.py
@@ -0,0 +1,7 @@
+# I am referring PYnative Python Programming website for practice from P037
+previous_num = 0
+
+for i in range(1, 11):
+    sum = previous_num + i
+    print("Current Number", i, "Previous Number ", previous_num, " Sum: ", previous_num + i)
+    previous_num = i
