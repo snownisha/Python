eee0e29059177671b3f5916e3ef392ad640e6fef
snownisha <snownisha123@gmail.com>
2022-09-03 08:43:37 +0200

    Creating new list

---
+++ P046_Creating New List.py
@@ -0,0 +1,17 @@
+def Merge(List1, List2):
+    result_list = []
+
+    # iterate first list
+    for num in List1:
+        if num % 2 != 0:
+            result_list.append(num)
+
+    for num in List2:
+        if num % 2 == 0:
+            result_list.append(num)
+    return result_list
+
+
+List1 = [10, 20, 25, 30, 35]
+List2 = [40, 45, 60, 75, 90]
+print("result list:", Merge(List1, List2))
