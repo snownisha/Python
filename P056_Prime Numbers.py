811f55ac3d273b05be874db83dd110c346480a88
snownisha <snownisha123@gmail.com>
2022-09-14 15:28:21 +0200

    Prime Numbers

---
+++ P056_Prime Numbers.py
@@ -0,0 +1,12 @@
+start = 25
+end = 50
+print("Prime numbers between", start, "and", end, "are:")
+
+for num in range(start, end + 1):
+    if num > 1:
+        for i in range(2, num):
+            # check for factors
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
