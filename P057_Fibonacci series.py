f823f34768d921908dee3e49b3d18fb4e0ff42bc
Snow Nisha Amala Doss <93483438+snownisha@users.noreply.github.com>
2022-09-17 13:10:26 +0200

    Update P057_Fibonacci series.py

--- P057_Fibonacci series.py
+++ P057_Fibonacci series.py
@@ -2,9 +2,7 @@
 num1, num2 = 0, 1
 print("Fibonacci sequence:")
 for i in range(10):
-    # print next number of a series
     print(num1, end="  ")
-    # add last two numbers to get next number
     res = num1 + num2
     num1 = num2
     num2 = res
