bf490f6990fe63a89df406432f7f925bc6915397
snownisha <snownisha123@gmail.com>
2022-09-17 13:06:58 +0200

    Factorial

---
+++ P058_factorial.py
@@ -0,0 +1,12 @@
+num = 5
+factorial = 1
+if num < 0:
+    print("Factorial does not exist for negative numbers")
+elif num == 0:
+    print("The factorial of 0 is 1")
+else:
+    # run loop 5 times
+    for i in range(1, num + 1):
+        # multiply factorial by current number
+        factorial = factorial * i
+    print("The factorial of", num, "is", factorial)
