afb5abaddb8fa7199bbee48cf9f451922db044e0
snownisha <snownisha123@gmail.com>
2022-11-19 01:24:31 +0100

    Count of Substring

--- P042_Count of Substring.py
+++ P042_Count of Substring.py
@@ -1,3 +1,4 @@
+#Count of Substring
 st= "Nisha is a girl. Nisha is from India and Nisha is currently a master student in offenburg university and Nisha stays in Elgersweier"
 StringCount = st.count("Nisha")
 print("Nisha is there " + str(StringCount)+ " " + "times in a sentence")
