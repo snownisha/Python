949f68567bfe4850750ab2d43d5b72e86cfd85c3
snownisha <snownisha123@gmail.com>
2022-11-19 01:21:07 +0100

    Current & Previous Number Sum

--- P037_Sum of Current and Previous Number.py
+++ P037_Sum of Current and Previous Number.py
@@ -1,4 +1,5 @@
-# I am referring PYnative Python Programming website for practice from P037
+# Sum of Current & Previous Number
+# I am referring PYnative Python Programming website for practice from P037 to P062
 previous_num = 0
 
 for i in range(1, 11):
